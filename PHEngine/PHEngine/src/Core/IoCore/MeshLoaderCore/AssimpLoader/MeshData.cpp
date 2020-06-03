20246383401e8f7a35d4b8adf71afa4794943e53
Dem <demusszinov@gmail.com>
2020-06-03 18:01:32 +0300

    removed unnecessary includes

--- PHEngine/PHEngine/src/Core/IoCore/MeshLoaderCore/AssimpLoader/MeshData.cpp
+++ PHEngine/PHEngine/src/Core/IoCore/MeshLoaderCore/AssimpLoader/MeshData.cpp
@@ -7,9 +7,7 @@
 #include <memory>
 #include <algorithm>
 
-#include <glm/gtc/quaternion.hpp>
 #include <glm/gtx/quaternion.hpp>
-#include <glm/gtx/matrix_decompose.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/compatibility.hpp>
 
