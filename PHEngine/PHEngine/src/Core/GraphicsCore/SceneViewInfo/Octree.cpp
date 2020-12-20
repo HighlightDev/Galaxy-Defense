e53dda7c43eaa0f54665372d29a18c0a2035e420
Dem <demusszinov@gmail.com>
2020-12-20 22:32:06 +0200

    Start of octree

---
+++ PHEngine/PHEngine/src/Core/GraphicsCore/SceneViewInfo/Octree.cpp
@@ -0,0 +1,15 @@
+#include "Octree.h"
+
+namespace Graphics
+{
+
+   Octree::Octree()
+   {
+   }
+
+
+   Octree::~Octree()
+   {
+   }
+
+}
---
+++ PHEngine/PHEngine/src/Core/GraphicsCore/SceneViewInfo/Octree.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Graphics
+{
+   struct OctreeNode
+   {
+      OctreeNode();
+      ~OctreeNode();
+   };
+
+   class Octree
+   {
+   public:
+      Octree();
+      ~Octree();
+   };
+
+}
+
