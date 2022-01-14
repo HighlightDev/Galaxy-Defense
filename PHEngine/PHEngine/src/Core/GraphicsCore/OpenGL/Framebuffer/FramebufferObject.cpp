038f4d8ba2f4389ee2f3d65b96a258748566c83a
Dmitriy <dzinoviev@luxoft.com>
2022-01-14 18:03:12 +0200

    bug: fixed bug with wrong  position of draw buffer attachment

--- PHEngine/PHEngine/src/Core/GraphicsCore/OpenGL/Framebuffer/FramebufferObject.cpp
+++ PHEngine/PHEngine/src/Core/GraphicsCore/OpenGL/Framebuffer/FramebufferObject.cpp
@@ -8,9 +8,7 @@ namespace Graphics
 {
 
    FramebufferObject::FramebufferObject()
-      : mFramebufferId(std::numeric_limits<uint32_t>::max())
-      , mRenderBufferId()
-      , mRenderTextures()
+       : mFramebufferId(std::numeric_limits<uint32_t>::max()), mRenderBufferId(), mRenderTextures()
    {
    }
 
@@ -30,9 +28,9 @@ namespace Graphics
       glGenFramebuffers(1, &mFramebufferId);
       glBindFramebuffer(GL_FRAMEBUFFER, mFramebufferId);
 
-      for (const auto& attachmentToTexture : mRenderTextures)
+      for (const auto &attachmentToTexture : mRenderTextures)
       {
-         const auto& renderTexture = attachmentToTexture.second;
+         const auto &renderTexture = attachmentToTexture.second;
          if (renderTexture->GetTextureType() == TextureType::TEXTURE_2D)
          {
             glFramebufferTexture2D(GL_FRAMEBUFFER, attachmentToTexture.first, GL_TEXTURE_2D, renderTexture->GetTextureDescriptor(), 0);
@@ -48,7 +46,8 @@ namespace Graphics
       CollectAttachments();
    }
 
-   void FramebufferObject::CheckErrors() {
+   void FramebufferObject::CheckErrors()
+   {
       glBindFramebuffer(GL_FRAMEBUFFER, mFramebufferId);
       GetFramebufferErrorCode();
    }
@@ -69,41 +68,80 @@ namespace Graphics
       GLenum errorCode = GetFramebufferErrorCode();
       switch (errorCode)
       {
-         case GL_FRAMEBUFFER_COMPLETE: { return "Framebuffer : complete."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT: { return "Framebuffer : Not all framebuffer attachment points are framebuffer attachment complete."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT: { return "Framebuffer : Not all attached images have the same width and height."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER: { return "Framebuffer : Each draw buffer must specify color attachment points that have images attached or must be GL_NONE."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT: { return "Framebuffer : Incomplete formats"; }
-         case GL_FRAMEBUFFER_INCOMPLETE_LAYER_COUNT_EXT: { return "Framebuffer : Incomplete layer count"; }
-         case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS: { return "Framebuffer : All attachments must be layered attachments."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT: { return "Framebuffer : No images are attached to the framebuffer."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE: { return "Framebuffer : All images must have the same number of multisample samples."; }
-         case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER: { return "Framebuffer : ReadBuffer must specify an attachment point that has an image attached. "; }
-         case GL_FRAMEBUFFER_UNDEFINED: { return "Framebuffer : FBO object number 0 is bound"; }
-         case GL_FRAMEBUFFER_UNSUPPORTED: { return "Framebuffer : The combination of internal formats of the attached images violates an implementation-dependent set of restrictions."; }
-         default: return "Framebuffer : Undefined error.";
+      case GL_FRAMEBUFFER_COMPLETE:
+      {
+         return "Framebuffer : complete.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+      {
+         return "Framebuffer : Not all framebuffer attachment points are framebuffer attachment complete.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS_EXT:
+      {
+         return "Framebuffer : Not all attached images have the same width and height.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+      {
+         return "Framebuffer : Each draw buffer must specify color attachment points that have images attached or must be GL_NONE.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_FORMATS_EXT:
+      {
+         return "Framebuffer : Incomplete formats";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_LAYER_COUNT_EXT:
+      {
+         return "Framebuffer : Incomplete layer count";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
+      {
+         return "Framebuffer : All attachments must be layered attachments.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+      {
+         return "Framebuffer : No images are attached to the framebuffer.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+      {
+         return "Framebuffer : All images must have the same number of multisample samples.";
+      }
+      case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+      {
+         return "Framebuffer : ReadBuffer must specify an attachment point that has an image attached. ";
+      }
+      case GL_FRAMEBUFFER_UNDEFINED:
+      {
+         return "Framebuffer : FBO object number 0 is bound";
+      }
+      case GL_FRAMEBUFFER_UNSUPPORTED:
+      {
+         return "Framebuffer : The combination of internal formats of the attached images violates an implementation-dependent set of restrictions.";
+      }
+      default:
+         return "Framebuffer : Undefined error.";
       }
    }
 
-
    void FramebufferObject::CollectAttachments()
    {
-      const size_t attachmentsCount = std::count_if(mRenderTextures.begin(), mRenderTextures.end(), [](const auto& pair) { return (pair.first != GL_DEPTH_ATTACHMENT && pair.first != GL_DEPTH_STENCIL_ATTACHMENT); });
+      const size_t attachmentsCount = std::count_if(mRenderTextures.begin(), mRenderTextures.end(), [](const auto &pair)
+                                                    { return (pair.first != GL_DEPTH_ATTACHMENT && pair.first != GL_DEPTH_STENCIL_ATTACHMENT); });
 
       if (attachmentsCount)
       {
          mFramebufferAttachments.reserve(attachmentsCount);
-         for (const auto& attachmentToTexture : mRenderTextures)
+         for (const auto &attachmentToTexture : mRenderTextures)
          {
             if (attachmentToTexture.first != GL_DEPTH_ATTACHMENT && attachmentToTexture.first != GL_DEPTH_STENCIL_ATTACHMENT)
                mFramebufferAttachments.push_back(attachmentToTexture.first);
          }
+         std::sort(mFramebufferAttachments.begin(), mFramebufferAttachments.end(), [](const auto &left, const auto &right) -> bool
+                   { return left < right; });
       }
    }
 
    // should be called after CreateFramebuffer method was called
    void FramebufferObject::CreateRenderBuffer(const int32_t renderbufferDataType, const int32_t framebufferRenderbufferAttachment,
-      const size_t screenResX, const size_t screenResY)
+                                              const size_t screenResX, const size_t screenResY)
    {
       assert(mFramebufferId != std::numeric_limits<uint32_t>::max());
 
@@ -116,7 +154,7 @@ namespace Graphics
    }
 
    void FramebufferObject::CreateRenderBuffer(const int32_t renderbufferDataType, const int32_t framebufferRenderbufferAttachment,
-      const glm::ivec2& screenResolution)
+                                              const glm::ivec2 &screenResolution)
    {
       assert(mFramebufferId != std::numeric_limits<uint32_t>::max());
 
